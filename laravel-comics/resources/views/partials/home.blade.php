a877f742680c66caeaa0b6d3ab26752cb7f50741
Nali1919 <my@email.com>
2022-10-30 12:48:33 +0100

    Completo parte content-yield

--- laravel-comics/resources/views/partials/home.blade.php
+++ laravel-comics/resources/views/partials/home.blade.php
@@ -2,6 +2,22 @@
 
 
 @section('content')
-    <div>CIAO</div>
+<div class="home-container">
+<div class="prodotti">
+ @foreach ($comics as $cc)
+     <div class="card">
+  <img src="{{ $cc['thumb'] }}" alt=" {{ $cc['description'] }}" />
+  <h5>{{$cc['series']}}</h5>
+    </div>
+
+     @endforeach
+
+
+</div>
+
+
+</div>
+
+
 @endsection
 
